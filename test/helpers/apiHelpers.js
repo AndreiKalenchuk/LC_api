19aee7fc6e6facf1a207f4be2e7d48e23a6c1e78
Andrei Kalenchuk <58866971+AndreiKalenchuk@users.noreply.github.com>
2020-08-06 18:57:36 -0400

    Tc-l054 profile/settings learner change pass (#3)
    * add costume method claarValue() --- wdio doesn't work
    ass user data learner credentials
    add learner apiLogin
    
    * TC-L054 Profile-->Settings learner change password
    add UI login method
    add UI logout method
    
    Co-authored-by: Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>

---
+++ test/helpers/apiHelpers.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+const host = "https://server-stage.pasv.us";
+
+export async function apiUserRegister(user) {
+  return axios
+    .post(`${host}/user`, {
+      email: user.email,
+      password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phone: user.phone,
+      about: user.about,
+      goals: user.goals,
+      englishLevel: user.englishLevel,
+      countryName: user.countryName,
+    })
+    .then((res) => res)
+    .catch((error) => error);
+}
+
+export function apiUserLogin(userRole) {
+  return axios
+    .post(`${host}/user/login`, {
+      email: userRole.email,
+      password: userRole.password,
+    })
+    .then((res) => res)
+    .catch((error) => error);
+}
--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,10 +1,7 @@
-
 module.exports = class Page {
-
-    open (path) {
-        return browser.url(path);
-        browser.maximizeWindow();
-    }
-
-    get header() { return $('h1') }
-}
+  open(path) {
+    return browser.url(path);
+    browser.maximizeWindow();
+    browser.deleteAllCookies();
+  }
+};
